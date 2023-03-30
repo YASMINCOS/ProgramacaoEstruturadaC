daf82e871dda5ffdbe3f0d8862a6b8e7a07d5bfe
Yasmin vitória costa <99223801+YASMINCOS@users.noreply.github.com>
2023-03-30 20:53:09 -0300

    Add files via upload

---
+++ aula2/exercicio1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+
+    printf("----------------------\n");
+    printf("Verificador de maior \n");
+    printf("-----------------------\n");
+
+    printf("Digite o primeiro número: \n");
+    scanf("%d", &num1);
+
+    printf("Digite o segundo número: \n");
+    scanf("%d", &num2);
+
+    if (num1 > num2) {
+        printf("%d é maior que %d", num1, num2);
+    } else if (num2 > num1) {
+        printf("%d é maior que %d", num2, num1);
+    } else {
+        printf("%d e %d são iguais", num1, num2);
+    }
+     printf("-----------------------\n");
+
+    return 0;
+}
